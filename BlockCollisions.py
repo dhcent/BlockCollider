9454903cb2e575ad75e9736e02d6b95e990f5074
Daniel Huang <huangdan0315@gmail.com>
2025-04-06 17:29:55 -0400

    a

--- BlockCollisions.py
+++ BlockCollisions.py
@@ -1,10 +1,26 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+from matplotlib.animation import FuncAnimation
 import numpy as np
 
 #linspace creates an array of length 5, evenly spacing numbers between stop & start
 x = np.linspace(0,2*np.pi,400)
 y = np.sin(x)
 fig, ax = plt.subplots()
-ax.plot(x,y)
+#Sets the x and y-axis
+ax.set_xlim(0,800)
+ax.set_ylim(0,800)
+#hides axis
+ax.set_axis_off()
 
+
+#(position, width, height)
+box_left = patches.Rectangle((100, 300), 50, 50, color="deepskyblue")
+box_right = patches.Rectangle((300, 300), 50, 50, color="orangered")
+#draws the boxes
+ax.add_patch(box_left)
+ax.add_patch(box_right)
+
+def update(frame):
+    
+plt.show()
